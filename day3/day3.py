127ad4ad3a58b38e99e7485d73646d4d8bf846d6
Liam Gower <lee.gower17@gmail.com>
2019-12-08 22:14:28 +0000

    Day 3 passed, day 4 looks tricky

--- day3/day3.py
+++ day3/day3.py
@@ -68,11 +68,36 @@ def get_shortest_intersect(route1, route2):
     intersections = set(route1) & set(route2)
     abs_intersections = [(abs(i[0]), abs(i[1])) for i in intersections]
 
-    return print(sum(min(abs_intersections, key=sum)))
+    # return print(sum(min(abs_intersections, key=sum)))
+    return intersections
+
+
+def get_wires_to_intersect(intersections, route1, route2):
+
+        rt1_indices = [(num, tupl) for num, tupl in enumerate(route1) if tupl in intersections]
+        rt2_indices = [(num, tupl) for num, tupl in enumerate(route2) if tupl in intersections]
+        route_sums = []
+
+        for num, tupl in enumerate(route1):
+
+            if tupl in intersections:
+
+                for num2, tupl2 in enumerate(route2):
+
+                    if tupl == tupl2:
+
+                        print("{} - {} and {} - {}".format(num, tupl, num2, tupl2))
+                        route_sums.append((num+num2 + 2, tupl))
+
+        return min(route_sums)[0]
 
 
 
 instructions1, instructions2 = read_input('input.txt')
 route1 = traverse_route(instructions1)
 route2 = traverse_route(instructions2)
-get_shortest_intersect(route1, route2)
+intersections = get_shortest_intersect(route1, route2)
+# get_shortest_intersect(route1, route2)
+
+test = get_wires_to_intersect(intersections, route1, route2)
+print(test)
---
+++ day4/day4.py
@@ -0,0 +1,23 @@
+test = '111111'
+test2 = '223450'
+test3 = '123789'
+
+start = 264793
+stop = 803935
+
+for i in test2:
+
+    # Greater than equal to start digit rule
+    if test2[i] < start[0]:
+        continue
+
+    #
+    elif test
+
+    #if digit >= start[0]:
+
+
+
+
+#continue
+
